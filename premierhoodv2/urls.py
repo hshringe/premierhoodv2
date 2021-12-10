c396022c910f71d25655e8cce802c7122e11bea8
Hardhi Shringeri <hardhi24@gmail.com>
2021-12-10 18:11:35 -0500

    added login

--- premierhoodv2/urls.py
+++ premierhoodv2/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import path, include # new
 
 urlpatterns = [
+    
     path('', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), 
 ]
